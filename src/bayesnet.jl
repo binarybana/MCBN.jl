4fd0279eb44b0cb7a62049c68880292e18f44c82
Jason Knight <Jason@JasonKnight.us>
2014-04-15 20:12:58 -0500

    Working and tested KLD and Entropy

--- src/bayesnet.jl
+++ src/bayesnet.jl
@@ -1,45 +1,14 @@
-
 export BayesNet, BayesNetSampler
 
 type Node 
     index::Int
     label::UTF8String
     var::Var
-    fac::Factor
-    function Node(i,l,v,f)
+    function Node(i,l,v)
         assert(i==label(v))
-        assert(v in vars(f))
-        new(i,l,v,f)
+        new(i,l,v)
     end
 end
-
-function show(io::IO, n::Node)
-    try
-        if int(n.label) == n.index
-            print(io, "$(n.index)")
-        end
-    catch
-        print(io, "$(n.index): $(n.label)")
-    end
-    print(io, " : [")
-    varvec = vars(vars((n.fac)))
-    for v in varvec
-        print(io, "$(label(v)),")
-    end
-    print(io, "\b] : [")
-    for i in 1:nrStates(n.fac)
-        @printf io "%.2f," n.fac[i]
-    end
-    print(io, "\b]")
-end
-
-function make_vertex(g::AdjacencyList{Node}, x) 
-    ind = num_vertices(g)+1
-    v = Var(ind,2) #FIXME hardcoded binary
-    Node(ind, string(x), v, Factor(v)) 
-end
-vertex_index(n::Node) = n.index
-
 typealias BayesNet AdjacencyList{Node}
 
 function BayesNet(n::Int)
@@ -54,13 +23,22 @@ function BayesNet(itr)
     g
 end
 
-function add_edge!(g::AdjacencyList{Node}, u::Int, v::Int)
-    un,vn = g.vertices[[u,v]]
-    vn.fac = embed(vn.fac, vars(vn.fac)+u.var)
-    add_edge!(g, un, vn)
+function show(io::IO, n::Node)
+    try
+        if int(n.label) == n.index
+            print(io, "$(n.index)")
+        end
+    catch
+        print(io, "$(n.index): $(n.label)")
+    end
 end
 
-function set_factor!(bnd::BayesNetDAI, i::Int, 
+function make_vertex(g::AdjacencyList{Node}, x) 
+    ind = num_vertices(g)+1
+    v = Var(ind,2) #FIXME hardcoded binary
+    Node(ind, string(x), v) 
+end
+vertex_index(n::Node) = n.index
 
 type BayesNetDAI
     bn::BayesNet
@@ -76,9 +54,41 @@ function BayesNetDAI(n::Int)
 end
 
 function BayesNetDAI(bn::BayesNet)
-    fg = FactorGraph([x.fac for x in bn.vertices]...)
+    fg = FactorGraph([Factor(x.var) for x in bn.vertices]...)
     jt = JTree(fg)
     BayesNetDAI(bn, fg, jt, true, -Inf)
+end
+
+function add_edge!(bnd::BayesNetDAI, u::Int, v::Int)
+    un,vn = bnd.bn.vertices[[u,v]]
+    vnfac = bnd.fg[v]
+    bnd.dirty=true
+    bnd.fg[v] = embed(vnfac, vars(vnfac)+un.var)
+    add_edge!(bnd.bn, un, vn)
+end
+
+function set_factor!(bnd::BayesNetDAI, i::Int, vals::Vector{Float64})
+    bnd.dirty = true
+    fcopy = bnd.fg[i]
+    assert(length(vals) == nrStates(fcopy))
+    oldvals = p(fcopy)
+    oldvals[:] = vals
+    bnd.fg[i] = fcopy
+end
+
+function show(io::IO, bnd::BayesNetDAI)
+    show(io, bnd.bn)
+    show(io, bnd.fg)
+    #print(io, " : [")
+    #varvec = vars(vars((n.fac)))
+    #for v in varvec
+        #print(io, "$(label(v)),")
+    #end
+    #print(io, "\b] : [")
+    #for i in 1:nrStates(n.fac)
+        #@printf io "%.2f," n.fac[i]
+    #end
+    #print(io, "\b]")
 end
 
 type BayesNetSampler
@@ -91,37 +101,25 @@ end
 #propose
 #energy
 #reject
- 
-function kld(bnd::BayesNetDAI, other::BayesNetDAI)
-    if !bnd.dirty
-        bnd.memo_entropy = entropy(bnd)
-        # Now entropy and jtree are correct
-    end
-    accum = 0.0
+
+function check_cpds(bnd::BayesNetDAI)
     for i=1:numFactors(bnd.fg)
-        v = other.bn.vertices[i].var
+        v = bnd.bn.vertices[i].var
         arity = states(v)
-        numparstates = iround(nrStates(other.fg[i])/arity)
-        # Assumption: same ordering between bnd and other
-        jointvars = vars(other.fg[i])
-        parvars = jointvars - v
-        margfac = marginal(bnd.jt, parvars)
-        joint = marginal(bnd.jt, jointvars)
+        parvars = vars(bnd.fg[i]) - v
+        numparstates = nrStates(parvars)
         for parstate=1:numparstates
-            subaccum = 0.0
+            tot = 0.0
             for state=1:arity
                 index = conditionalState(v, parvars, state, parstate)
-                p_x_par = other.fg[i][index]
-                q_condprob = joint[index]/margfac[parstate]
-                subaccum += q_condprob * log2(p_x_par)
+                tot += bnd.fg[i][index]
             end
-            accum += margfac[parstate] * subaccum
+            Base.Test.@test_approx_eq tot 1.0
         end
     end
-    return -bnd.memo_entropy - accum
 end
- 
-function faster_kld(bnd::BayesNetDAI, other::BayesNetDAI)
+
+function kld(bnd::BayesNetDAI, other::BayesNetDAI)
     if !bnd.dirty
         bnd.memo_entropy = entropy(bnd)
         # Now entropy and jtree are correct
@@ -150,6 +148,7 @@ function entropy(bnd::BayesNetDAI)
     if !bnd.dirty
         return bnd.memo_entropy
     end
+    bnd.jt = JTree(bnd.fg)
     init!(bnd.jt)
     run!(bnd.jt)
     bnd.dirty = false
@@ -157,15 +156,18 @@ function entropy(bnd::BayesNetDAI)
     for i in 1:numFactors(bnd.fg)
         v = bnd.bn.vertices[i]
         arity = states(v.var)
-        numparstates = iround(nrStates(bnd.fg[i])/arity)
         pars = vars(bnd.fg[i]) - v.var
+        numparstates = nrStates(pars)
         margfac = marginal(bnd.jt, pars)
         for parstate in 1:numparstates
             accumsum = 0.0
+            tot = 0.0
             for state in 1:arity
                 temp = bnd.fg[i][conditionalState(v.var, pars, state, parstate)]
                 accumsum -= temp == 0.0 ? 0.0 : temp * log2(temp)
+                tot += temp
             end
+            #Base.Test.@test_approx_eq tot 1.0
             accum += margfac[parstate] * accumsum
         end
     end
@@ -176,10 +178,12 @@ end
 function naive_entropy(bnd::BayesNetDAI)
     numstates = 1
     accum = 0.0
+    tot = 0.0
     allvars = VarSet([x.var for x=bnd.bn.vertices]...)
     for n in bnd.bn.vertices
         numstates *= states(n.var)
     end
+    assert(numstates == nrStates(allvars))
 
     for i=1:numstates
         accumprod = 1.0
@@ -188,8 +192,10 @@ function naive_entropy(bnd::BayesNetDAI)
             reducedstate = calcLinearState(vars(bnd.fg[j]), statemap, allvars)
             accumprod *= bnd.fg[j][reducedstate]
         end
+        tot += accumprod
         accum += accumprod == 0 ? 0.0 : accumprod * log2(accumprod)
     end
+    assert((tot-1.0)<=eps())
     return -accum
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,17 +2,54 @@ reload("MCBN")
 using Base.Test  
 
 # write your own tests here
-#g = MCBN.BayesNet(5)
 bnd = MCBN.BayesNetDAI(5)
 bnd2 = MCBN.BayesNetDAI(5)
 bnd3 = MCBN.BayesNetDAI(5)
 
+
 @test_approx_eq MCBN.entropy(bnd) 5.0
 @test_approx_eq MCBN.naive_entropy(bnd) 5.0
-@test_approx_eq MCBN.naive_entropy(bnd) MCBN.entropy(bnd)
+@test_approx_eq MCBN.kld(bnd,bnd2) 0.0
 
-@show k1 = MCBN.kld(bnd,bnd2)
-@show k2 = MCBN.faster_kld(bnd,bnd2)
+function test_net(bnd)
+    MCBN.check_cpds(bnd)
+    @test_approx_eq MCBN.naive_entropy(bnd) MCBN.entropy(bnd)
+    @test_approx_eq MCBN.kld(bnd,bnd) 0.0
+end
+
+function test_kld(bnd1, bnd2)
+    kldval = MCBN.kld(bnd1, bnd2)
+    kldvalrev = MCBN.kld(bnd2, bnd1)
+    @test kldval >= 0
+    @test kldvalrev >= 0
+end
+
+test_net(bnd)
+test_kld(bnd, bnd2)
+
+MCBN.add_edge!(bnd3, 1,2)
+#MCBN.check_cpds(bnd3)
+
+MCBN.set_factor!(bnd3, 2, [1.0, 0.5, 0.0, 0.5])
+MCBN.set_factor!(bnd3, 1, [0.8, 0.2])
+MCBN.check_cpds(bnd3)
+
+@test_approx_eq MCBN.entropy(bnd3) 3.9219280948873623
+@test_approx_eq MCBN.entropy(bnd3) MCBN.naive_entropy(bnd3)
+
+test_kld(bnd, bnd3)
+
+MCBN.restoreFactors!(bnd3.fg)
+bnd3.dirty = true
+
+@test_approx_eq MCBN.entropy(bnd3) MCBN.naive_entropy(bnd3)
+@test_approx_eq MCBN.entropy(bnd3) 5.0
+
+@test_approx_eq MCBN.kld(bnd,bnd3) 0.0
+@test_approx_eq MCBN.kld(bnd3,bnd) 0.0
+
+test_net(bnd3)
+test_kld(bnd, bnd3)
 
     #empty = BayesNetCPD(np.array([2,2,2]))
     #assert empty.entropy() == 3.0
